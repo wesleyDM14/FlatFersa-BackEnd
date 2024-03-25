8c11575c572837966cf520c21bc765abff506928
wesleyDM14 <wesleyfga@hotmail.com>
2024-03-25 10:10:40 -0300

    Reset all concepts

--- src/middlewares/isAuthenticated.ts
+++ src/middlewares/isAuthenticated.ts
@@ -1,39 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
+import prismaClient from '../prisma';
 
 interface Payload {
     sub: string;
-    admin: boolean;
-}
-
-export function isAuthenticated(
-    req: Request,
-    res: Response,
-    next: NextFunction
-) {
-    //receber o token
-    const authToken = req.headers.authorization;
-
-    if (!authToken) {
-        return res.status(401).end();
-    }
-
-    const [, token] = authToken.split(" ");
-
-    try {
-        //validar token
-        const validation = verify(
-            token,
-            process.env.JWT_SECRET
-        ) as Payload;
-
-        req.user_id = validation.sub;
-        req.user_admin = validation.admin;
-
-        return next();
-    } catch (err) {
-        return res.status(401).end();
-    }
 }
 
 export function isLoggedIn(
@@ -61,6 +31,26 @@ export function isLoggedIn(
 
         return next();
     } catch (err) {
-        return res.status(401).end();
+        return res.status(401).json({ message: 'Token de Acesso Inválido' });
+    }
+}
+
+export async function isAdmin(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    try {
+        const user = await prismaClient.user.findFirst({ where: { id: req.user_id } });
+
+        if (!user) {
+            return res.status(404).json({ message: 'Usuário não Encontrado' });
+        }
+
+        if (!user.isAdmin) {
+            return res.status(403).json({ message: 'Acessp negado: permissões insuficientes' });
+        }
+    } catch (error) {
+
     }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,36 +1,5 @@
 import { Router } from 'express';
 
-import { CreateUserController } from './controllers/user/CreateUserController';
-import { AuthUserController } from './controllers/user/AuthUserController';
-import { DetailUserController } from './controllers/user/DetailUserController';
-import { CreateClientController } from './controllers/cliente/CreateClientController';
-
-import { isAuthenticated } from './middlewares/isAuthenticated';
-import { DetailClientController } from './controllers/cliente/DetailClientController';
-import { CreateApartmentController } from './controllers/apartment/CreateApartmentController';
-import { DetailApartmentController } from './controllers/apartment/DetailApartmentController';
-import { DeleteClientController } from './controllers/cliente/DeleteClientController';
-import { DeleteApartmentController } from './controllers/apartment/DeleteApartmentController';
-import { DeleteUserController } from './controllers/user/DeleteUserController';
-
 const router = Router();
-
-// -- Rotas User --
-router.post('/users', new CreateUserController().handle);
-router.get('/users', isAuthenticated, new DetailUserController().getAllUsers);
-router.post('/getUser', isAuthenticated, new DetailUserController().getUserById);
-router.post('/session', new AuthUserController().handle);
-router.get('/me', isAuthenticated, new DetailUserController().getLoggedIn);
-router.post('/deleteUser', isAuthenticated, new DeleteUserController().handle);
-
-// -- Rotas Client --
-router.post('/clients', isAuthenticated, new CreateClientController().handle);
-router.get('/clients', isAuthenticated, new DetailClientController().getAll);
-router.post('/deleteClient', isAuthenticated, new DeleteClientController().handle);
-
-// -- Rotas Apartment --
-router.post('/apartments', isAuthenticated, new CreateApartmentController().handle);
-router.get('/apartments', isAuthenticated, new DetailApartmentController().getAll);
-router.post('/deleteApartment', isAuthenticated, new DeleteApartmentController().handle);
 
 export { router };
